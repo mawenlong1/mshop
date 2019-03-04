47d728fde2b79d2876dd679f5fa7171a50b86529
mawenlong <15061886113@163.com>
2019-03-04 19:46:45 +0800

    商品管理中心

---
+++ mshop-provider/mshop-provider-cmc/src/main/java/com/mwl/mshop/provider/cmc/MshopProviderCmcApplication.java
@@ -0,0 +1,13 @@
+package com.mwl.mshop.provider.cmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MshopProviderCmcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MshopProviderCmcApplication.class, args);
+    }
+
+}
