b812acd732e0d962ff6cf0909a8cf08cdac42104
mawenlong <mwljndx@163.com>
2019-03-08 21:57:14 +0800

    service层

--- mshop-provider/mshop-provider-cmc/src/main/java/com/mwl/mshop/provider/cmc/service/impl/CmcBrandServiceImpl.java
+++ mshop-provider/mshop-provider-cmc/src/main/java/com/mwl/mshop/provider/cmc/service/impl/CmcBrandServiceImpl.java
@@ -1,12 +1,19 @@
 package com.mwl.mshop.provider.cmc.service.impl;
 
+import com.mwl.mshop.provider.cmc.mapper.CmcBrandMapper;
 import com.mwl.mshop.provider.cmc.service.CmcBrandService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 /**
  * @author mawenlong
  * @date 2019-03-06 23:54
  */
 @Slf4j
+@Service
 public class CmcBrandServiceImpl implements CmcBrandService {
+
+    private CmcBrandMapper brandMapper;
+
+
 }
